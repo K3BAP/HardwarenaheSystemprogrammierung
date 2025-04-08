c5a40ebb5727e89d382de6812413fab9d728323a
Fabian Sponholz <s4faspon@uni-trier.de>
2025-04-08 20:47:14 +0200

    Create statistics for second experiment

--- abschlussprojekt/statistics/statistics.R
+++ abschlussprojekt/statistics/statistics.R
@@ -93,5 +93,98 @@ boxplot(data,
         col = "lightgreen")
 dev.off()
 
+#### Intel OpenCL ####
 
-# TODO
+data_file <- "../results/intel/opencl/intel_opencl_matmul.txt"
+data <- as.numeric(readLines(data_file))
+str(data)
+
+summary(data)
+
+# Create a histogram
+png("intel/opencl/histogram_matmul.png")
+hist(data,
+     main = "Intel: OpenCL Matrix Multiplication",
+     xlab = "Runtime",
+     col = "skyblue", 
+     border = "black")
+dev.off()
+
+# Create a box plot
+png("intel/opencl/boxplot_matmul.png")
+boxplot(data,
+        main = "Intel: OpenCL Matrix Multiplication",
+        ylab = "Runtime",
+        col = "lightgreen")
+dev.off()
+
+data_file <- "../results/intel/opencl/intel_opencl_copy.txt"
+data <- as.numeric(readLines(data_file))
+str(data)
+
+summary(data)
+
+# Create a histogram
+png("intel/opencl/histogram_copy.png")
+hist(data,
+     main = "Intel: OpenCL Copy Kernel",
+     xlab = "Runtime",
+     col = "skyblue", 
+     border = "black")
+dev.off()
+
+# Create a box plot
+png("intel/opencl/boxplot_copy.png")
+boxplot(data,
+        main = "Intel: OpenCL Copy Kernel",
+        ylab = "Runtime",
+        col = "lightgreen")
+dev.off()
+
+#### Intel oneAPI ####
+
+data_file <- "../results/intel/oneapi/intel_oneapi_matmul.txt"
+data <- as.numeric(readLines(data_file))
+str(data)
+
+summary(data)
+
+# Create a histogram
+png("intel/oneapi/histogram_matmul.png")
+hist(data,
+     main = "Intel: OneAPI Matrix Multiplication",
+     xlab = "Runtime",
+     col = "skyblue", 
+     border = "black")
+dev.off()
+
+# Create a box plot
+png("intel/oneapi/boxplot_matmul.png")
+boxplot(data,
+        main = "Intel: OneAPI Matrix Multiplication",
+        ylab = "Runtime",
+        col = "lightgreen")
+dev.off()
+
+data_file <- "../results/intel/oneapi/intel_oneapi_copy.txt"
+data <- as.numeric(readLines(data_file))
+str(data)
+
+summary(data)
+
+# Create a histogram
+png("intel/oneapi/histogram_copy.png")
+hist(data,
+     main = "Intel: OneAPI Copy Kernel",
+     xlab = "Runtime",
+     col = "skyblue", 
+     border = "black")
+dev.off()
+
+# Create a box plot
+png("intel/oneapi/boxplot_copy.png")
+boxplot(data,
+        main = "Intel: OneAPI Copy Kernel",
+        ylab = "Runtime",
+        col = "lightgreen")
+dev.off()
